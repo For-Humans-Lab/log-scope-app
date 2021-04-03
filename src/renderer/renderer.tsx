3e8c0c44a663581592990d070bf0f00b3fb58372
UnkwUsr <ktoto2707043@gmail.com>
2021-04-03 23:50:03 +0300

    Reading stdout of subprocess

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -7,30 +7,63 @@ import '_public/style.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const { spawn } = require('child_process');
+import { spawn } from 'child_process';
 
-function App(props: {}) {
-    const [out, setOut] = React.useState<string[]>([])
+interface DataLine {
+    linenumber: number,
+    data: string
+}
 
-    React.useEffect(() => {
-        const ls = spawn('npx', ['run', 'start'], { cwd: __dirname + '../' }) as any;
-        console.log(__dirname)
-        ls.stdout.on('data', (data: Buffer) => {
-            setOut([...out, data.toString()])
+function showOut(out: DataLine[]) {
+    if (out.length == 0)
+        return (<div>Program not running</div>)
+    else
+        return (
+            <div>
+                Program out:
+                <div>
+                    {out.map(p => <><b>{p.linenumber}</b>. <div>{p.data}</div> </>)}
+                </div>
+            </div>
+        )
+}
+
+function App() {
+    const [out, setOut] = React.useState<DataLine[]>([])
+
+    function run() {
+        var linenumber: number = 0
+
+        const process = spawn('./emulate_realtime_stdout.sh', ['log_tin_example', '10'])
+
+        process.stdout.on('data', (data: Buffer) => {
+            var lines = data.toString().split('\n')
+            var datalines: DataLine[] = []
+            for (var i in lines) {
+                if (lines[i].length == 0)
+                    continue
+
+                linenumber++
+                datalines.push({ linenumber, data: lines[i] })
+            }
+
+            setOut((oldout) => [...oldout, ...datalines])
+            // console.log(data.toString().split('\n'))
         });
 
-        ls.stderr.on('data', (data: Buffer) => {
+        process.stderr.on('data', (data: Buffer) => {
             console.error(`stderr: ${data}`);
         });
 
-        ls.on('close', (code: any) => {
-            console.log(`child process exited with code ${code}`);
+        process.on('close', (_code: any) => {
+            setOut([])
         });
-    }, [])
+    }
 
     return (
         <div>
-            {out.map(p => <div>{p}</div>)}
+            <div><button onClick={run}>Run</button></div>
+            {showOut(out)}
         </div>
     )
 }
@@ -39,3 +72,4 @@ function App(props: {}) {
 ReactDOM.render(<App />,
     document.getElementById('app'),
 );
+

9db4a29737cc5389d595669ac747c2177128899d
UnkwUsr <ktoto2707043@gmail.com>
2021-04-03 17:18:45 +0300

    Apply formating

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -10,32 +10,32 @@ import * as ReactDOM from 'react-dom';
 const { spawn } = require('child_process');
 
 function App(props: {}) {
-  const [out, setOut] = React.useState<string[]>([])
+    const [out, setOut] = React.useState<string[]>([])
 
-  React.useEffect(() => {
-    const ls = spawn('npx', ['run', 'start'], {cwd:__dirname+'../'}) as any;
-    console.log(__dirname)
-    ls.stdout.on('data', (data: Buffer) => {
-      setOut([...out, data.toString()])
-    });
+    React.useEffect(() => {
+        const ls = spawn('npx', ['run', 'start'], { cwd: __dirname + '../' }) as any;
+        console.log(__dirname)
+        ls.stdout.on('data', (data: Buffer) => {
+            setOut([...out, data.toString()])
+        });
 
-    ls.stderr.on('data', (data: Buffer) => {
-      console.error(`stderr: ${data}`);
-    });
+        ls.stderr.on('data', (data: Buffer) => {
+            console.error(`stderr: ${data}`);
+        });
 
-    ls.on('close', (code: any) => {
-      console.log(`child process exited with code ${code}`);
-    });
-  }, [])
+        ls.on('close', (code: any) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }, [])
 
-  return (
-    <div>
-      {out.map(p=><div>{p}</div>)}
-    </div>
-  )
+    return (
+        <div>
+            {out.map(p => <div>{p}</div>)}
+        </div>
+    )
 }
 
 
 ReactDOM.render(<App />,
-  document.getElementById('app'),
+    document.getElementById('app'),
 );

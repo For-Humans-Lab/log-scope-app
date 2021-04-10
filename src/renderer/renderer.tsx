3a4f904d8f645388bf847e57897049ff5c92fc27
Vladislav Sabenin <vasabenin@gmail.com>
2021-04-10 21:00:20 +0300

    loint bug

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -227,6 +227,7 @@ function App() {
               <ReloadIcon style={{ color: "gray", fontSize: 18 }} />
             </MenuBarButton>
 
+
             <MenuBarButton onClick={()=>{setLogEntries([])}}>
               <ClearIcon style={{ color: "gray" }} />
             </MenuBarButton>
